5b11afdd03a2c8877a36e18294719da46096c8ba
Tyler Nguyen <nguyenhtyler@gmail.com>
2024-12-21 02:28:22 -0500

    Add accessibility features

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,16 @@
-import { useState } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import Die from './Die'
 import {nanoid} from 'nanoid'
 import Confetti from 'react-confetti'
 
 export default function App() {
     const [dice, setDice] = useState(() => generateAllNewDice());
+    const buttonRef = useRef(null)
     const gameWon = dice.every(die => die.isHeld && dice[0].value === die.value);
+
+    useEffect(() => {
+        buttonRef.current.focus()
+    }, [gameWon])
 
     function rollDie() {
         return Math.ceil(Math.random() * 6);
@@ -43,12 +48,15 @@ export default function App() {
     return (
         <main>
             {gameWon && <Confetti />}
+            <div aria-label="polite" className="sr-only">
+                {gameWon && <p>Congratulations! You won! Press "New Game" to start again.</p>}
+            </div>
             <h1 className="title">Tenzies</h1>
             <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
             <div id="diceDiv">
                 {diceElements}
             </div>
-            <button onClick={rollDice} id="rollBtn">{gameWon ? "New Game" : "Roll"}</button>
+            <button ref={buttonRef} onClick={rollDice} id="rollBtn">{gameWon ? "New Game" : "Roll"}</button>
         </main>
     )
 }
--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -3,6 +3,12 @@ export default function Die(props) {
         backgroundColor: props.isHeld ? "#59E391" : "White"
     }
     return (
-        <button style={styles} onClick={props.hold} className={"die"}>{props.value}</button>
+        <button
+            style={styles}
+            onClick={props.hold} 
+            className={"die"}
+            aria-pressed={props.isHeld}
+            aria-label={`Die with value ${props.value}, ${props.isHeld ? "held" : "not held"}`}
+        >{props.value}</button>
     )
 }

d14ce0918996db7e075c9306901dae128a0c6de9
Tyler Nguyen <nguyenhtyler@gmail.com>
2024-12-21 00:15:36 -0500

    Init dice

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,20 @@
+import Die from './Die'
+
 export default function App() {
     return(
         <main>
-            
+            <div id="diceDiv">
+                <Die value={1}/>
+                <Die value={1}/>
+                <Die value={1}/>
+                <Die value={1}/>
+                <Die value={1}/>
+                <Die value={1}/>
+                <Die value={1}/>
+                <Die value={1}/>
+                <Die value={1}/>
+                <Die value={1}/>
+            </div>
         </main>
     )
 }

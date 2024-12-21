d65367d5bcc35c5a468336127671d13f22abfa82
Tyler Nguyen <nguyenhtyler@gmail.com>
2024-12-21 00:31:45 -0500

    Add roll functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,6 +4,10 @@ import Die from './Die'
 export default function App() {
     const [dice, setDice] = useState(generateAllNewDice());
 
+    function rollDice() {
+        setDice(generateAllNewDice);
+    }
+
     function generateAllNewDice() {
         return new Array(10)
             .fill(0)
@@ -17,6 +21,7 @@ export default function App() {
             <div id="diceDiv">
                 {diceElements}
             </div>
+            <button onClick={rollDice} id="rollBtn">Roll</button>
         </main>
     )
 }

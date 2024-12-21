92f248040be93cb1f89a5c0de834eafa89c6a1b9
Tyler Nguyen <nguyenhtyler@gmail.com>
2024-12-21 00:37:03 -0500

    Improve roll button UI

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,14 +4,14 @@ import Die from './Die'
 export default function App() {
     const [dice, setDice] = useState(generateAllNewDice());
 
-    function rollDice() {
-        setDice(generateAllNewDice);
-    }
-
     function generateAllNewDice() {
         return new Array(10)
             .fill(0)
             .map(() => Math.ceil(Math.random() * 6))
+    }
+
+    function rollDice() {
+        setDice(generateAllNewDice);
     }
 
     const diceElements = dice.map(num => <Die value={num}/>)

e06f3c3070bf957a3009f3a69b595bf89e50dfca
Tyler Nguyen <nguyenhtyler@gmail.com>
2024-12-21 00:28:21 -0500

    Randomize dice

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,21 @@
+import { useState } from 'react'
 import Die from './Die'
 
 export default function App() {
-    return(
+    const [dice, setDice] = useState(generateAllNewDice());
+
+    function generateAllNewDice() {
+        return new Array(10)
+            .fill(0)
+            .map(() => Math.ceil(Math.random() * 6))
+    }
+
+    const diceElements = dice.map(num => <Die value={num}/>)
+
+    return (
         <main>
             <div id="diceDiv">
-                <Die value={1}/>
-                <Die value={1}/>
-                <Die value={1}/>
-                <Die value={1}/>
-                <Die value={1}/>
-                <Die value={1}/>
-                <Die value={1}/>
-                <Die value={1}/>
-                <Die value={1}/>
-                <Die value={1}/>
+                {diceElements}
             </div>
         </main>
     )

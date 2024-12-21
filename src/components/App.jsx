163f08920cd209b153b4d2a0149f5f1d9ab2b9d5
Tyler Nguyen <nguyenhtyler@gmail.com>
2024-12-21 01:35:51 -0500

    Add title and instructions

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -36,6 +36,8 @@ export default function App() {
 
     return (
         <main>
+            <h1 className="title">Tenzies</h1>
+            <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
             <div id="diceDiv">
                 {diceElements}
             </div>
